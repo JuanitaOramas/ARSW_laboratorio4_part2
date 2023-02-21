9b6f5143737b977167a5c8ce7efa5cc7ba90e119
JuanitaO <juanitaoramas@gmail.com>
2023-02-21 12:57:51 -0500

    updateTests

--- src/test/java/edu/eci/arsw/blueprints/test/persistence/impl/FilterTest.java
+++ src/test/java/edu/eci/arsw/blueprints/test/persistence/impl/FilterTest.java
@@ -15,19 +15,25 @@ public class FilterTest {
 
     @Test
     public void shouldApplyRedundancyFilter() throws BlueprintPersistenceException, BlueprintNotFoundException {
+        InMemoryBlueprintPersistence bpp =new InMemoryBlueprintPersistence();
 
-        Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15),new Point(15, 15), new Point(40, 40),  new Point(50, 05)};
-
-
-        Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
+        Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15),new Point(10, 12)};
         Filter f = new RedundancyFilter();
+        Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
+        bpp.saveBlueprint(bp0);
+
+        Blueprint blueprintResult = bpp.getBlueprint("mack", "mypaint");
+        blueprintResult = f.applyFilter(blueprintResult);
+        Point[] pts1 =new Point[]{new Point(40, 40)};
+
 //        BlueprintsServices bps = new BlueprintsServices();
 //        bps.addNewBlueprint(bp0);
 //        Blueprint result = bps.getBlueprint(bp0.getAuthor(), bp0.getName());
-        Blueprint result = f.applyFilter(bp0);
-        int sizeResult = result.getPoints().size();
 
-        assertEquals(3,sizeResult);
+
+        assertNotNull("Loading a points stored blueprint returned not null.", blueprintResult.getPoints());
+        assertEquals("Points filtered by Redundancy", blueprintResult.getPoints().size(), 3);
+
 
     }
 
